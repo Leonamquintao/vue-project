b15d3183dbea5517ffd4e74e91237261a32ed6a9
Leonamquintao <leonamquintao@gmail.com>
2018-01-31 21:13:28 -0200

    first commit

---
+++ src/components/home-component.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="home">
+
+    <h1 v-once>{{ msg }}</h1>
+    <p>{{ msg }} - <a v-bind:href="link">Google</a></p>
+    <p>{{ sayHello() }} - <a :href="link">Google</a></p>
+
+    <hr>
+    <p>{{ finishedLink }}</p>
+    <p v-html="finishedLink"></p>
+
+    <hr>
+    <button v-on:click="increase()">Add +1 to Counter</button>
+    <button v-on:click="decrease()">Remove 1 to Counter</button>
+    <p>Counter: {{ counter }}</p>
+
+    <hr>
+    <p v-on:mousemove="updateCoordenates($event)">Coordenates: {{ x }} / {{ y }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'HomeComponent',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App!!!',
+        link: 'http://google.com',
+        finishedLink: '<a href="http://google.com">Google</a>',
+        counter: 0,
+        x: 0,
+        y: 0,
+
+      }
+    },
+    methods: {
+      sayHello() {
+        this.msg = 'Hello!';
+        return this.msg;
+      },
+      increase() {
+        this.counter++;
+      },
+      decrease() {
+        if(this.counter > 0) {
+          this.counter--;
+        }
+      },
+      updateCoordenates(event) {
+        this.x = event.clientX;
+        this.y = event.clientY;
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
